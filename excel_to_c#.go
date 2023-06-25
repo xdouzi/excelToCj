f0314dba42a58ffca4fca75c45b5a191c7448f49
yh <408309839@qq.com>
2023-06-25 17:37:40 +0800

    fix

---
+++ excel_to_c#.go
@@ -0,0 +1,45 @@
+/*
+*excel_to_c#
+added by yh @ 2023/6/25 17:35
+注意:
+*/
+package main
+
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+	f, err := excelize.OpenFile("Book1.xlsx")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	// Get value from cell by given worksheet name and cell reference.
+	cell, err := f.GetCellValue("Sheet1", "B2")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(cell)
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, row := range rows {
+		for _, colCell := range row {
+			fmt.Print(colCell, "\t")
+		}
+		fmt.Println()
+	}
+}
